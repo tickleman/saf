0a3277ed3d7fa37055cbdf78e32b591d970b6e74
Baptiste <baptiste.pillot@bappli.com>
2012-02-17 09:59:20 +0100

    Rename CollectionName annotation to DataSet, remove unused ColumnType

--- src/com/bappli/saf/datalink/sql/SqlTable.java
+++ src/com/bappli/saf/datalink/sql/SqlTable.java
@@ -3,7 +3,7 @@ package com.bappli.saf.datalink.sql;
 import java.sql.SQLException;
 import java.util.Map;
 
-import com.bappli.saf.annotations.CollectionName;
+import com.bappli.saf.annotations.DataSet;
 import com.bappli.saf.datalink.DataField;
 
 public abstract class SqlTable
@@ -17,8 +17,8 @@ public abstract class SqlTable
 	//------------------------------------------------------------------------------ classToTableName
 	public static String classToTableName(Class<? extends Object> objectClass)
 	{
-		if (objectClass.getAnnotation(CollectionName.class) != null) {
-			return objectClass.getAnnotation(CollectionName.class).value().toLowerCase();
+		if (objectClass.getAnnotation(DataSet.class) != null) {
+			return objectClass.getAnnotation(DataSet.class).value().toLowerCase();
 		} else {
 			String className = objectClass.getSimpleName();
 			if (className.substring(className.length() - 1).equals("y")) {
